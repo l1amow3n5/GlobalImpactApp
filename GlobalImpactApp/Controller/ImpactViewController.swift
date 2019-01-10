cad7a9c2df1309424f30a865ea479190c20e3c44
Liam Owen <l1amow3n5@yahoo.com>
2019-01-10 12:15:11 -0700

    Initial commit

---
+++ GlobalImpactApp/Controller/ImpactViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GlobalImpactApp
+//
+//  Created by Owen, Liam on 1/10/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ImpactViewController: UIViewController
+{
+
+   public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
