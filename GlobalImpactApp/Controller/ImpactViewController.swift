3463da8a0c3c9a6227f8ffb345b1c883f4e730e0
Liam Owen <l1amow3n5@yahoo.com>
2019-01-10 12:40:07 -0700

    added the code for the button and for the extension

--- GlobalImpactApp/Controller/ImpactViewController.swift
+++ GlobalImpactApp/Controller/ImpactViewController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 public class ImpactViewController: UIViewController
 {
-
+    @IBOutlet weak var MapView: MKMapView!
+    @IBAction func MapButton(_ sender: Any)
+    {
+        let impactLocation = CLLocationCoordinate2D(latitude: 1, longitude: 1)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 1.05, longitudeDelta: 10.05)
+        let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+        MapView.animatedZoom(zoomRegion: impactRegion, duration: 2.5)
+    
+    }
+    
+    
    public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
@@ -18,5 +29,14 @@ public class ImpactViewController: UIViewController
     }
 
 
+}
+
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion:MKCoordinateRegion, duration: TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations:
+            { self.setRegion(zoomRegion, animated: true)}, completion: nil)
+    }
 }
 
